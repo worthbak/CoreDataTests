0ba5f86125a7b19c93d0b7a7e64f78316e563712
Worth Baker <wbaker@greenmtd.com>
2015-12-21 13:18:23 -0700

    Initial Commit

---
+++ CDTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CDTests
+//
+//  Created by David Baker on 12/21/15.
+//  Copyright © 2015 Worth Baker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
